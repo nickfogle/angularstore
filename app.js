7fbd8d68b892ee518caea9bb3e34ebcd50625bcc
nickfogle <nickfogle@gmail.com>
2014-05-28 23:23:57 -0400

    added media and other assets

--- app.js
+++ app.js
@@ -9,25 +9,45 @@ var gems = [
       name: "Dodecahedron",
       price: 21.00,
       description: "One dose and gravity is all nonsense. oppressive forces that flicker in and out of view like ambassadors from the kind of non-Euclidean occult geometries that drive people insane in H. P. Lovecraft's short stories.",
-      canPurchase: true,
+      images: [
+         {
+            full: 'Dodecahedron-full.jpg',
+            thumb: 'Dodecahedron-thumb.jpg'
+         },
+      ]
    },
    {
       name: "Tetrahedron",
       price: 44.05,
       description: "Increased neural distribution and broader gamma waves. Three doses are all you need to enhanced IQ.",
-      canPurchase: false,
+      images: [
+         {
+            full: 'Tetrahedron-full.jpg',
+            thumb: 'Tetrahedron-thumb.jpg'
+         },
+      ]
    },
    {
       name: "Pyrophosphate",
       price: 37.99,
       description: "Illegal in 49 States. Synthesized from the bowels of Cherynobyl's quarantine zone. Potent and very effective.",
-      canPurchase: true,
+      images: [
+         {
+            full: 'Pyrophosphate-full.jpg',
+            thumb: 'Pyrophosphate-thumb.jpg'
+         },
+      ]
    },
    {
       name: "Modafinale",
       price: 222.95,
       description: "Prescription quality available without a prescription. A fusion of nootropics ready for the next-generation of cerebral cocktails.",
-      canPurchase: true,
+      images: [
+         {
+            full: 'Modafinale-full.jpg',
+            thumb: 'Modafinale-thumb.jpg'
+         },
+      ]
    },
 ];
 })();
